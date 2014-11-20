c051b91f8959f406bb3491a4ef5610edde3b6834
Pete Brooks <peterossbrooks@gmail.com>
2014-11-20 16:38:41 -0600

    separated gloves + now visible on every page

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,16 +2,29 @@
 
 angular.module('dnApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-
-    $scope.isCollapsed = true;
-
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-
-    console.log($location.path());
+    var current = $location.path().substring(1);
+    console.log(current);
+    $scope.isRouteActive = function(view) {
+        // console.log($location + ": " + current);
+        if (current === '') {
+          return '';
+        } else {
+          return view === current ? 'active' : 'inactive';
+        };
+    }
+    $scope.active = undefined;
+    $scope.setActive = function(name) {
+      $scope.active = name;
+    }
+    $scope.isActive = function(name) {
+      if ($scope.active) {
+        if (name === $scope.active) {
+          return 'active'
+        } else {
+          return 'inactive'
+        }
+      } else {
+        return true
+      }
+    }
   });
