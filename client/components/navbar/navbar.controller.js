2ba9976f65f46a10b80ed42980aec9b36ee745ff
Pete Brooks <peterossbrooks@gmail.com>
2014-11-20 17:57:25 -0600

    incorporated title into glove directive + new problem with route updates

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,26 +4,26 @@ angular.module('dnApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.isRouteActive = function(view) {
       var current = $location.path().substring(1);
-      // console.log(current);
+      console.log(current);
       if (current === '') {
         return 'home';
       } else {
         return view === current ? 'active' : 'inactive';
       };
     }
-    $scope.active = undefined;
-    $scope.setActive = function(name) {
-      $scope.active = name;
-    }
-    $scope.isActive = function(name) {
-      if ($scope.active) {
-        if (name === $scope.active) {
-          return 'active'
-        } else {
-          return 'inactive'
-        }
-      } else {
-        return true
-      }
-    }
+    // $scope.active = undefined;
+    // $scope.setActive = function(name) {
+    //   $scope.active = name;
+    // }
+    // $scope.isActive = function(name) {
+    //   if ($scope.active) {
+    //     if (name === $scope.active) {
+    //       return 'active'
+    //     } else {
+    //       return 'inactive'
+    //     }
+    //   } else {
+    //     return true
+    //   }
+    // }
   });
