e6395b0ff1549e68363f370cae35d4f7c7ad5985
Pete Brooks <peterossbrooks@gmail.com>
2014-11-22 22:54:05 -0600

    added all gloves + main styles

---
+++ server/config/photos_collection.js
@@ -0,0 +1,17 @@
+var photos_collection =
+[
+{
+  _id: "", // MUST BE UNIQUE
+  filepath: "assets/images/{{ FILENAME }}",
+  description: "",
+  type: "thumb / main / detail",
+  work_id:
+},
+{
+  _id: "", // MUST BE UNIQUE
+  filepath: "assets/images/{{ FILENAME }}",
+  description: "",
+  type: "thumb / main / detail",
+  work_id:
+}
+]
---
+++ server/config/works_collection.js
@@ -0,0 +1,20 @@
+var works_collection =
+  [
+  {
+    _id: , // MUST BE UNIQUE
+    title: "",
+    year: ,
+    description: "",
+    media: "",
+    dimensions: "",
+    photos: []
+  },
+  {
+    _id: ,
+    title: "",
+    year: ,
+    description: "",
+    media: "",
+    dimensions: "",
+    photos: []
+  },
