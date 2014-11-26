340f4297eb5d99e5c139f000a14a70b274530908
Pete Brooks <peterossbrooks@gmail.com>
2014-11-26 16:21:38 -0600

    mega nav + works stylin

--- server/config/photos_collection.js
+++ server/config/photos_collection.js
@@ -60,29 +60,29 @@ var photos_collection =
 //   work_id: 'mattress'
 // },
 
-{
-  filepath: 'assets/images/a_plastic_case-main.jpg',
-  type: 'main',
-  work_id: 'a_plastic_case'
-},
+// {
+//   filepath: 'assets/images/a_plastic_case-main.jpg',
+//   type: 'main',
+//   work_id: 'a_plastic_case'
+// },
 
-{
-  filepath: 'assets/images/a_plastic_case-detail-1.jpg',
-  type: 'detail',
-  work_id: 'a_plastic_case'
-},
+// {
+//   filepath: 'assets/images/a_plastic_case-detail-1.jpg',
+//   type: 'detail',
+//   work_id: 'a_plastic_case'
+// },
 
-{
-  filepath: 'assets/images/a_plastic_case-detail-2.jpg',
-  type: 'detail',
-  work_id: 'a_plastic_case'
-},
+// {
+//   filepath: 'assets/images/a_plastic_case-detail-2.jpg',
+//   type: 'detail',
+//   work_id: 'a_plastic_case'
+// },
 
-{
-  filepath: 'assets/images/a_plastic_case-detail-3.jpg',
-  type: 'detail',
-  work_id: 'a_plastic_case'
-},
+// {
+//   filepath: 'assets/images/a_plastic_case-detail-3.jpg',
+//   type: 'detail',
+//   work_id: 'a_plastic_case'
+// },
 
 // {
 //   filepath: 'assets/images/a_plastic_case-thumb.jpg',
@@ -90,11 +90,11 @@ var photos_collection =
 //   work_id: 'a_plastic_case'
 // },
 
-{
-  filepath: 'assets/images/pearl_apron_split-main.jpg',
-  type: 'main',
-  work_id: 'pearl_apron_split'
-},
+// {
+//   filepath: 'assets/images/pearl_apron_split-main.jpg',
+//   type: 'main',
+//   work_id: 'pearl_apron_split'
+// },
 
 // {
 //   filepath: 'assets/images/pearl_apron_split-thumb.jpg',
@@ -102,11 +102,11 @@ var photos_collection =
 //   work_id: 'pearl_apron_split'
 // },
 
-{
-  filepath: 'assets/images/diagram-main.jpg',
-  type: 'main',
-  work_id: 'diagram'
-},
+// {
+//   filepath: 'assets/images/diagram-main.jpg',
+//   type: 'main',
+//   work_id: 'diagram'
+// },
 
 // {
 //   filepath: 'assets/images/diagram-thumb.jpg',
@@ -114,11 +114,11 @@ var photos_collection =
 //   work_id: 'diagram'
 // },
 
-{
-  filepath: 'assets/images/untitled-main.jpg',
-  type: 'main',
-  work_id: 'untitled'
-},
+// {
+//   filepath: 'assets/images/untitled-main.jpg',
+//   type: 'main',
+//   work_id: 'untitled'
+// },
 
 // {
 //   filepath: 'assets/images/untitled-thumb.jpg',
@@ -126,17 +126,17 @@ var photos_collection =
 //   work_id: 'untitled'
 // },
 
-{
-  filepath: 'assets/images/a_spot-main.jpg',
-  type: 'main',
-  work_id: 'a_spot'
-},
+// {
+//   filepath: 'assets/images/a_spot-main.jpg',
+//   type: 'main',
+//   work_id: 'a_spot'
+// },
 
-{
-  filepath: 'assets/images/a_spot-detail.jpg',
-  type: 'detail',
-  work_id: 'a_spot'
-},
+// {
+//   filepath: 'assets/images/a_spot-detail.jpg',
+//   type: 'detail',
+//   work_id: 'a_spot'
+// },
 
 // {
 //   filepath: 'assets/images/a_spot-thumb.jpg',
@@ -144,23 +144,23 @@ var photos_collection =
 //   work_id: 'a_spot'
 // },
 
-{
-  filepath: 'assets/images/vromb-main.jpg',
-  type: 'main',
-  work_id: 'vromb'
-},
+// {
+//   filepath: 'assets/images/vromb-main.jpg',
+//   type: 'main',
+//   work_id: 'vromb'
+// },
 
-{
-  filepath: 'assets/images/vromb-detail-1.jpg',
-  type: 'detail',
-  work_id: 'vromb'
-},
+// {
+//   filepath: 'assets/images/vromb-detail-1.jpg',
+//   type: 'detail',
+//   work_id: 'vromb'
+// },
 
-{
-  filepath: 'assets/images/vromb-detail-2.jpg',
-  type: 'detail',
-  work_id: 'vromb'
-},
+// {
+//   filepath: 'assets/images/vromb-detail-2.jpg',
+//   type: 'detail',
+//   work_id: 'vromb'
+// },
 
 // {
 //   filepath: 'assets/images/vromb-thumb.jpg',
@@ -168,11 +168,11 @@ var photos_collection =
 //   work_id: 'vromb'
 // },
 
-{
-  filepath: 'assets/images/rachel-main.jpg',
-  type: 'main',
-  work_id: 'rachel'
-},
+// {
+//   filepath: 'assets/images/rachel-main.jpg',
+//   type: 'main',
+//   work_id: 'rachel'
+// },
 
 // {
 //   filepath: 'assets/images/rachel-thumb.jpg',
@@ -180,11 +180,11 @@ var photos_collection =
 //   work_id: 'rachel'
 // },
 
-{
-  filepath: 'assets/images/pete-main.jpg',
-  type: 'main',
-  work_id: 'pete'
-},
+// {
+//   filepath: 'assets/images/pete-main.jpg',
+//   type: 'main',
+//   work_id: 'pete'
+// },
 
 // {
 //   filepath: 'assets/images/pete-thumb.jpg',
@@ -192,11 +192,11 @@ var photos_collection =
 //   work_id: 'pete'
 // },
 
-{
-  filepath: 'assets/images/xtina-main.jpg',
-  type: 'main',
-  work_id: 'xtina'
-},
+// {
+//   filepath: 'assets/images/xtina-main.jpg',
+//   type: 'main',
+//   work_id: 'xtina'
+// },
 
 // {
 //   filepath: 'assets/images/xtina-thumb.jpg',
@@ -204,23 +204,23 @@ var photos_collection =
 //   work_id: 'xtina'
 // },
 
-{
-  filepath: 'assets/images/unlimited_intimacy-main.jpg',
-  type: 'main',
-  work_id: 'unlimited_intimacy'
-},
+// {
+//   filepath: 'assets/images/unlimited_intimacy-main.jpg',
+//   type: 'main',
+//   work_id: 'unlimited_intimacy'
+// },
 
-{
-  filepath: 'assets/images/unlimited_intimacy-detail-1.jpg',
-  type: 'detail',
-  work_id: 'unlimited_intimacy'
-},
+// {
+//   filepath: 'assets/images/unlimited_intimacy-detail-1.jpg',
+//   type: 'detail',
+//   work_id: 'unlimited_intimacy'
+// },
 
-{
-  filepath: 'assets/images/unlimited_intimacy-detail-2.jpg',
-  type: 'detail',
-  work_id: 'unlimited_intimacy'
-},
+// {
+//   filepath: 'assets/images/unlimited_intimacy-detail-2.jpg',
+//   type: 'detail',
+//   work_id: 'unlimited_intimacy'
+// },
 
 // {
 //   filepath: 'assets/images/unlimited_intimacy-thumb.jpg',
@@ -228,17 +228,17 @@ var photos_collection =
 //   work_id: 'unlimited_intimacy'
 // },
 
-{
-  filepath: 'assets/images/quick_release_object-main.jpg',
-  type: 'main',
-  work_id: 'quick_release_object'
-},
+// {
+//   filepath: 'assets/images/quick_release_object-main.jpg',
+//   type: 'main',
+//   work_id: 'quick_release_object'
+// },
 
-{
-  filepath: 'assets/images/quick_release_object-detail.jpg',
-  type: 'detail',
-  work_id: 'quick_release_object'
-},
+// {
+//   filepath: 'assets/images/quick_release_object-detail.jpg',
+//   type: 'detail',
+//   work_id: 'quick_release_object'
+// },
 
 // {
 //   filepath: 'assets/images/quick_release_object-thumb.jpg',
@@ -246,11 +246,11 @@ var photos_collection =
 //   work_id: 'quick_release_object'
 // },
 
-{
-  filepath: 'assets/images/it_is_what_it_isnt-main.jpg',
-  type: 'main',
-  work_id: 'it_is_what_it_isnt'
-},
+// {
+//   filepath: 'assets/images/it_is_what_it_isnt-main.jpg',
+//   type: 'main',
+//   work_id: 'it_is_what_it_isnt'
+// },
 
 // {
 //   filepath: 'assets/images/it_is_what_it_isnt-thumb.jpg',
@@ -258,17 +258,17 @@ var photos_collection =
 //   work_id: 'it_is_what_it_isnt'
 // },
 
-{
-  filepath: 'assets/images/come_on_down-main.jpg',
-  type: 'main',
-  work_id: 'come_on_down'
-},
+// {
+//   filepath: 'assets/images/come_on_down-main.jpg',
+//   type: 'main',
+//   work_id: 'come_on_down'
+// },
 
-{
-  filepath: 'assets/images/come_on_down-detail.jpg',
-  type: 'detail',
-  work_id: 'come_on_down'
-},
+// {
+//   filepath: 'assets/images/come_on_down-detail.jpg',
+//   type: 'detail',
+//   work_id: 'come_on_down'
+// },
 
 // {
 //   filepath: 'assets/images/come_on_down-thumb.jpg',
@@ -276,11 +276,11 @@ var photos_collection =
 //   work_id: 'come_on_down'
 // },
 
-{
-  filepath: 'assets/images/not_a_grave-main.jpg',
-  type: 'main',
-  work_id: 'not_a_grave'
-},
+// {
+//   filepath: 'assets/images/not_a_grave-main.jpg',
+//   type: 'main',
+//   work_id: 'not_a_grave'
+// },
 
 // {
 //   filepath: 'assets/images/not_a_grave-thumb.jpg',
@@ -288,11 +288,11 @@ var photos_collection =
 //   work_id: 'not_a_grave'
 // },
 
-{
-  filepath: 'assets/images/bonding_suits-main.jpg',
-  type: 'main',
-  work_id: 'bonding_suits'
-},
+// {
+//   filepath: 'assets/images/bonding_suits-main.jpg',
+//   type: 'main',
+//   work_id: 'bonding_suits'
+// },
 
 // {
 //   filepath: 'assets/images/bonding_suits-thumb.jpg',
@@ -300,11 +300,11 @@ var photos_collection =
 //   work_id: 'bonding_suits'
 // },
 
-{
-  filepath: 'assets/images/fall_for_it-main.jpg',
-  type: 'main',
-  work_id: 'fall_for_it'
-},
+// {
+//   filepath: 'assets/images/fall_for_it-main.jpg',
+//   type: 'main',
+//   work_id: 'fall_for_it'
+// },
 
 // {
 //   filepath: 'assets/images/fall_for_it-thumb.jpg',
@@ -312,11 +312,11 @@ var photos_collection =
 //   work_id: 'fall_for_it'
 // },
 
-{
-  filepath: 'assets/images/anatomy_school-main.jpg',
-  type: 'main',
-  work_id: 'anatomy_school'
-},
+// {
+//   filepath: 'assets/images/anatomy_school-main.jpg',
+//   type: 'main',
+//   work_id: 'anatomy_school'
+// },
 
 // {
 //   filepath: 'assets/images/anatomy_school-thumb.jpg',
@@ -324,11 +324,11 @@ var photos_collection =
 //   work_id: 'anatomy_school'
 // },
 
-{
-  filepath: 'assets/images/monkey-main.jpg',
-  type: 'main',
-  work_id: 'monkey'
-},
+// {
+//   filepath: 'assets/images/monkey-main.jpg',
+//   type: 'main',
+//   work_id: 'monkey'
+// },
 
 // {
 //   filepath: 'assets/images/monkey-thumb.jpg',
@@ -336,17 +336,17 @@ var photos_collection =
 //   work_id: 'monkey'
 // },
 
-{
-  filepath: 'assets/images/naughty_teddy-main.jpg',
-  type: 'main',
-  work_id: 'naughty_teddy'
-},
+// {
+//   filepath: 'assets/images/naughty_teddy-main.jpg',
+//   type: 'main',
+//   work_id: 'naughty_teddy'
+// },
 
-{
-  filepath: 'assets/images/naughty_teddy-detail.jpg',
-  type: 'detail',
-  work_id: 'naughty_teddy'
-},
+// {
+//   filepath: 'assets/images/naughty_teddy-detail.jpg',
+//   type: 'detail',
+//   work_id: 'naughty_teddy'
+// },
 
 // {
 //   filepath: 'assets/images/naughty_teddy-thumb.jpg',
@@ -354,11 +354,11 @@ var photos_collection =
 //   work_id: 'naughty_teddy'
 // },
 
-{
-  filepath: 'assets/images/tumbleweed-main.jpg',
-  type: 'main',
-  work_id: 'tumbleweed'
-},
+// {
+//   filepath: 'assets/images/tumbleweed-main.jpg',
+//   type: 'main',
+//   work_id: 'tumbleweed'
+// },
 
 // {
 //   filepath: 'assets/images/tumbleweed-thumb.jpg',
@@ -366,17 +366,17 @@ var photos_collection =
 //   work_id: 'tumbleweed'
 // },
 
-{
-  filepath: 'assets/images/saddle-main.jpg',
-  type: 'main',
-  work_id: 'saddle'
-},
+// {
+//   filepath: 'assets/images/saddle-main.jpg',
+//   type: 'main',
+//   work_id: 'saddle'
+// },
 
-{
-  filepath: 'assets/images/saddle-detail.jpg',
-  type: 'detail',
-  work_id: 'saddle'
-},
+// {
+//   filepath: 'assets/images/saddle-detail.jpg',
+//   type: 'detail',
+//   work_id: 'saddle'
+// },
 
 // {
 //   filepath: 'assets/images/saddle-thumb.jpg',
@@ -384,35 +384,35 @@ var photos_collection =
 //   work_id: 'saddle'
 // },
 
-{
-  filepath: 'assets/images/pagemaster-main.jpg',
-  type: 'main',
-  work_id: 'pagemaster'
-},
+// {
+//   filepath: 'assets/images/pagemaster-main.jpg',
+//   type: 'main',
+//   work_id: 'pagemaster'
+// },
 
-{
-  filepath: 'assets/images/pagemaster-detail.jpg',
-  type: 'detail',
-  work_id: 'pagemaster'
-},
+// {
+//   filepath: 'assets/images/pagemaster-detail.jpg',
+//   type: 'detail',
+//   work_id: 'pagemaster'
+// },
 
-{
-  filepath: 'assets/images/inferno_scenes-main.jpg',
-  type: 'main',
-  work_id: 'inferno_scenes'
-},
+// {
+//   filepath: 'assets/images/inferno_scenes-main.jpg',
+//   type: 'main',
+//   work_id: 'inferno_scenes'
+// },
 
-{
-  filepath: 'assets/images/inferno_scenes-detail.jpg',
-  type: 'detail',
-  work_id: 'inferno_scenes'
-},
+// {
+//   filepath: 'assets/images/inferno_scenes-detail.jpg',
+//   type: 'detail',
+//   work_id: 'inferno_scenes'
+// },
 
-{
-  filepath: 'assets/images/inferno_scenes-thumb.jpg',
-  type: 'thumb',
-  work_id: 'inferno_scenes'
-}
+// {
+//   filepath: 'assets/images/inferno_scenes-thumb.jpg',
+//   type: 'thumb',
+//   work_id: 'inferno_scenes'
+// }
 ];
 var photos_module = function() {
   return photos_collection;
